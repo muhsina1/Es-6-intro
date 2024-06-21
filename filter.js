98ad4938b553d7a22dfed3bfd48aebe19e832778
muhsina1 <muhsinamawaddah8709@gmail.com>
2024-06-21 16:34:38 +0600

    filter added

--- filter.js
+++ filter.js
@@ -1,3 +1,7 @@
 const players = [5, 60, 50, 40, 10, 9, 4, 2]
 const selected =players.filter(n=> n>15)
 console.log(selected)
+
+const nameList =['mitali', 'Mawaddah', 'Rubi', 'Selima', 'Mitu']
+const fourUp =nameList.filter(name=> name.length>4)
+console.log(fourUp)
